c63c804695e07db3143bb523c22370f91de2e8d5
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-09-17 16:25:12 +0530

    Time: 286 ms (22.82%), Space: 14.9 MB (21.58%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -2,23 +2,37 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int r=matrix.size(),c=matrix[0].size();
-        for(int i=0;i<r;i++){
-            if(target>=matrix[i][0] && target<=matrix[i][c-1]){
-                int l=0,rt=c-1;
-                while(l<=rt){
-                    int mid=(l+rt)/2;
-                    if(target==matrix[i][mid]){
-                        return true;
-                    }
-                    if(target>matrix[i][mid]){
-                        l=mid+1;
-                    }
-                    else{
-                        rt=mid-1;
-                    }
-                }
+        int i=0,j=c-1;
+        while(i<r && j>=0){
+            if(target==matrix[i][j]){
+                return true;
+            }
+            if(target>matrix[i][j]){
+                i++;
+            }
+            else{
+                j--;
             }
         }
         return false;
+        
+        // for(int i=0;i<r;i++){
+        //     if(target>=matrix[i][0] && target<=matrix[i][c-1]){
+        //         int l=0,rt=c-1;
+        //         while(l<=rt){
+        //             int mid=(l+rt)/2;
+        //             if(target==matrix[i][mid]){
+        //                 return true;
+        //             }
+        //             if(target>matrix[i][mid]){
+        //                 l=mid+1;
+        //             }
+        //             else{
+        //                 rt=mid-1;
+        //             }
+        //         }
+        //     }
+        // }
+        // return false;
     }
 };
