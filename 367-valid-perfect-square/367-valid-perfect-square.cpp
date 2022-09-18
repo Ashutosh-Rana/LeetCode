ed24811686ce870f4b91e8dd8e6c7316ac35605b
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-09-18 18:06:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (17.20%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        double d=sqrt(num);
-        d*=100;
-        int r=(int)d%100;
-        if(r==0){
-            return true;
+        // double d=sqrt(num);
+        // d*=100;
+        // int r=(int)d%100;
+        // if(r==0){
+        //     return true;
+        // }
+        // return false;
+        
+        int i=1;
+        while(num>0){
+            num-=i;
+            i+=2;
+            if(!num){
+                return true;
+            }
         }
         return false;
     }
