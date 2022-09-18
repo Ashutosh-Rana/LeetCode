1ebfb095589d1cbcd936adc4c84683c6423f46ca
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-09-18 18:40:57 +0530

    Time: 15 ms (28.98%), Space: 5.8 MB (69.49%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        unsigned i=1,sum=0;
+        while(sum<n){
+            sum+=i;
+            i++;
+        }
+        if(sum>n){
+            return i-2;
+        }
+        return i-1;
+    }
+};
