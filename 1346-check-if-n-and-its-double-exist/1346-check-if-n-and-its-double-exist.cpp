c211311b48c30a7edc8ad5898e8282fdaa2b8307
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-09-19 16:47:10 +0530

    Time: 18 ms (23.89%), Space: 9.9 MB (93.74%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -8,6 +8,13 @@ public:
                 }
             }
         }
+        // unordered_set<int> s;
+        // for(int i=0;i<arr.size();i++){
+        //     if(s.count(2*arr[i])>0 || (arr[i]%2==0 && s.count(arr[i]/2)>0)){
+        //         return true;
+        //     }
+        //     s.insert(arr[i]);
+        // }
         return false;
     }
 };
