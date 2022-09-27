6ec4b09864f057d96de98c63acdea28c12e96b94
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-09-27 19:44:01 +0530

    Time: 3 ms (76.70%), Space: 8.6 MB (5.09%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int n=nums.size(),cnt=0,sum=0,i=0;
-        sort(nums.begin(),nums.end());
-        int max=nums[n-1];
-        while(i<n && sum!=max){
-            if(nums[i]==0 || (i>0 && nums[i]==nums[i-1])){
-                i++;
-                continue;
+        // int n=nums.size(),cnt=0,sum=0,i=0;
+        // sort(nums.begin(),nums.end());
+        // int max=nums[n-1];
+        // while(i<n && sum!=max){
+        //     if(nums[i]==0 || (i>0 && nums[i]==nums[i-1])){
+        //         i++;
+        //         continue;
+        //     }
+        //     if(i!=0){
+        //         sum+=nums[i]-nums[i-1];
+        //     }
+        //     else{
+        //         sum+=nums[i];
+        //     }
+        //     cnt++;
+        //     i++;
+        // }
+        // return cnt;
+        unordered_set<int> s(nums.begin(),nums.end());
+        int cnt=0;
+        for(int a:s){
+            if(a){
+                cnt++;
             }
-            if(i!=0){
-                sum+=nums[i]-nums[i-1];
-            }
-            else{
-                sum+=nums[i];
-            }
-            cnt++;
-            i++;
         }
         return cnt;
     }
