84f9b4ac02f2c8d07de4a7220514da648dd82332
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-09-29 12:50:29 +0530

    Time: 491 ms (5.03%), Space: 214.3 MB (11.55%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,16 +1,36 @@
 class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(int a:nums){
-            pq.push(a);
-            if(pq.size()>k){
-                pq.pop();
+private:
+    int quickSelect(int k,int l,int r,vector<int> nums){
+        int p=l,pivot=nums[r];
+        for(int i=l;i<r;i++){
+            if(nums[i]<pivot){
+                swap(nums[i],nums[p]);
+                p++;
             }
         }
-        // for(int i=0;i<k-1;i++){
-        //     pq.pop();
+        swap(nums[p],nums[r]);
+        if(k>p){
+            return quickSelect(k,p+1,r,nums);
+        }
+        else if(k<p){
+            return quickSelect(k,l,p-1,nums);
+        }
+        else{
+            return nums[p];
+        }
+    }
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // priority_queue<int,vector<int>,greater<int>> pq;
+        // for(int a:nums){
+        //     pq.push(a);
+        //     if(pq.size()>k){
+        //         pq.pop();
+        //     }
         // }
-        return pq.top();
+        // return pq.top();
+        k=nums.size()-k;
+        int res=quickSelect(k,0,nums.size()-1,nums);
+        return res;
     }
 };
