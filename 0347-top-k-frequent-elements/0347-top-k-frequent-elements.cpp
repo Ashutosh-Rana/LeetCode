f9343300876c7759ec9f52427ed9c3006bae877d
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-10-17 20:59:10 +0530

    Time: 71 ms (5.04%), Space: 20.4 MB (8.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,23 +2,43 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int> res;
-        int inf=numeric_limits<int> :: min();
+        //int inf=numeric_limits<int> :: min();
         unordered_map<int,int> mp;
         for(int a:nums){
             mp[a]++;
         }
-        multimap<int,int,greater<int>> mm;
+        // multimap<int,int,greater<int>> mm;
+        // for(auto it:mp){
+        //     mm.insert({it.second,it.first});
+        // }
+        // int cnt=0;
+        // for(auto it2:mm){
+        //     res.push_back(it2.second);
+        //     cnt++;
+        //     if(cnt==k){
+        //         break;
+        //     }
+        // }
+        
+        unordered_map<int,vector<int>> freq;
+        // for(int i=1;i<=nums.size();i++){
+        //     freq[i]=[];
+        // }
         for(auto it:mp){
-            mm.insert({it.second,it.first});
+            freq[it.second].push_back(it.first);
         }
-        int cnt=0;
-        for(auto it2:mm){
-            res.push_back(it2.second);
-            cnt++;
-            if(cnt==k){
+        for(int i=nums.size();i>=1;i--){
+            for(int j=0;j<freq[i].size();j++){
+                res.push_back(freq[i][j]);
+                if(res.size()==k){
+                    break;
+                }
+            }
+            if(res.size()==k){
                 break;
             }
         }
+        
         return res;
     }
 };
