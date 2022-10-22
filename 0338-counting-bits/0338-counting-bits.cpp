f9baff86a3a5fb34d89f90eaa63e29e19629a4c0
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-10-23 01:41:00 +0530

    Time: 7 ms (78.87%), Space: 7.8 MB (73.42%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1);
+        if(n==0){
+            return {0};
+        }
+        dp[0]=0,dp[1]=1;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                dp[i]=dp[i/2];
+            }
+            else{
+                dp[i]=dp[i/2]+dp[i%2];
+            }
+        }
+        return dp;
+    }
+};
