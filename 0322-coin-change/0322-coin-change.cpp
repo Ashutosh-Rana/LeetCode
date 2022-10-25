2d8793f207d4626c5270a13d7dff4077db4c1984
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-10-25 17:15:57 +0530

    Time: 128 ms (71.67%), Space: 14.6 MB (56.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,26 +4,28 @@ public:
         if(amount==0){
             return 0;
         }
-        vector<int> dp(amount+1,INT_MAX);
+        int Max=amount+1;
+        vector<int> dp(amount+1,Max);
         dp[0]=0;
         for(int i=0;i<coins.size();i++){
             int st=coins[i];
             for(int j=st;j<=amount;j++){
-                if(dp[j-st]==INT_MAX && dp[j]==INT_MAX){
-                    dp[j]=INT_MAX;
-                }
-                else if(dp[j-st]==INT_MAX){
-                    continue;
-                }
-                else if(dp[j]==INT_MAX){
-                    dp[j]=dp[j-st]+1;
-                }
-                else{
-                    dp[j]=min(dp[j],dp[j-st]+1);
-                }
+                dp[j]=min(dp[j],dp[j-st]+1);
+                // if(dp[j-st]==INT_MAX && dp[j]==INT_MAX){
+                //     dp[j]=INT_MAX;
+                // }
+                // else if(dp[j-st]==INT_MAX){
+                //     continue;
+                // }
+                // else if(dp[j]==INT_MAX){
+                //     dp[j]=dp[j-st]+1;
+                // }
+                // else{
+                //     dp[j]=min(dp[j],dp[j-st]+1);
+                // }
             }
         }
-        if(dp[amount]==INT_MAX){
+        if(dp[amount]>amount){
             return -1;
         }
         return dp[amount];
