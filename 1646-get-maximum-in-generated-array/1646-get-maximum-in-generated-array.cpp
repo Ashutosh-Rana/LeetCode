42fb1b6488ea53fe7499ddf0e90a41320ab80005
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-11-01 00:26:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.90%) - LeetHub

---
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if(n==0){
+            return 0;
+        }
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            if(i%2==0){
+                dp[i]=dp[i/2];
+            }
+            else{
+                dp[i]=dp[i/2]+dp[(i/2)+1];
+            }
+        }
+        int Max=0;
+        for(int a:dp){
+            Max=max(a,Max);
+        }
+        return Max;
+    }
+};
