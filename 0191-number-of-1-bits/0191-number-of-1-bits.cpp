628a8fe1fd500f1a61c51c5bfc5869554bd5dd07
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-11-02 20:00:06 +0530

    Time: 11 ms (7.63%), Space: 5.8 MB (79.07%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,7 +3,10 @@ public:
     int hammingWeight(uint32_t n) {
         int cnt=0;
         while(n){
-            if(n&1==1){
+            // if(n&1==1){
+            //     cnt++;
+            // } or
+            if(n%2==1){
                 cnt++;
             }
             n=n>>1;//right shift
