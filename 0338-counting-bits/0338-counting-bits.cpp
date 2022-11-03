1f594e72b79928ff7926479a8f2a8f2eef621b9e
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-11-03 18:55:49 +0530

    Time: 10 ms (55.49%), Space: 7.7 MB (88.32%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        // vector<int> dp(n+1);
-        // if(n==0){
-        //     return {0};
-        // }
-        // dp[0]=0,dp[1]=1;
-        // for(int i=1;i<=n;i++){
-        //     if(i%2==0){
-        //         dp[i]=dp[i/2];
-        //     }
-        //     else{
-        //         dp[i]=dp[i/2]+dp[i%2];
-        //     }
-        // }
-        // return dp;
-        
-        vector<int> res(n+1);
-        res[0]=0;
-        for(int i=1;i<=n;i++){
-            int cnt=0;
-            int j=i;
-            // while(j){
-            //     j=j&(j-1);
-            //     cnt++;
-            // }
-            // while(j){
-            //     cnt+=j&1;
-            //     j=j>>1;
-            // }
-            while(j){
-                cnt+=j%2;
-                j/=2;
-            }
-            res[i]=cnt;
+        vector<int> dp(n+1);
+        if(n==0){
+            return {0};
         }
-        return res;
+        dp[0]=0,dp[1]=1;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                dp[i]=dp[i/2];
+            }
+            else{
+                dp[i]=dp[i/2]+dp[i%2];
+            }
+        }
+        return dp;
+        
+        // vector<int> res(n+1);
+        // res[0]=0;
+        // for(int i=1;i<=n;i++){
+        //     int cnt=0;
+        //     int j=i;
+        //     // while(j){
+        //     //     j=j&(j-1);
+        //     //     cnt++;
+        //     // }
+        //     // while(j){
+        //     //     cnt+=j&1;
+        //     //     j=j>>1;
+        //     // }
+        //     while(j){
+        //         cnt+=j%2;
+        //         j/=2;
+        //     }
+        //     res[i]=cnt;
+        // }
+        // return res;
     }
 };
