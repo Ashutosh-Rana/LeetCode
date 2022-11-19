805ca677d581d7e86eb9a3cae30e4142efd75463
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2022-11-19 23:44:04 +0530

    Time: 82 ms (94.34%), Space: 26.9 MB (90.57%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         sort(hand.begin(),hand.end());
-        int cnt=1,ele=hand[0],n=hand.size();
-        unordered_map<int,bool> vis;
+        int n=hand.size();
         unordered_map<int,int> mp;
         for(int a:hand){
             mp[a]++;
@@ -14,27 +13,6 @@ public:
         if(groupSize==1){
             return true;
         }
-//         for(int i=1;i<n;i++){
-//             if(cnt==0 && vis[i]==false){
-//                 vis[i]=true;
-//                 cnt++;
-//                 ele=hand[i];
-//             }
-//             else{
-//                 if(vis[i]==false && (hand[i]-ele==1)){
-//                     cnt++;
-//                     vis[i]=true;
-//                     if(cnt==groupSize){
-//                         cnt=0;
-//                         i=0;
-//                     }
-//                     else{
-//                         ele=hand[i];
-//                     }
-                    
-//                 }
-//             }
-//         }
         for(int a:hand){
             if(mp[a]){
                 int ele=a,flag=1;
@@ -47,11 +25,6 @@ public:
                         return false;
                     }
                 }
-                // if(flag){
-                //     for(int i=0;i<groupSize;i++){
-                //         mp[ele+i]--;
-                //     }
-                // }
             }
         }
         for(auto it:mp){
@@ -60,9 +33,5 @@ public:
             }
         }
         return true;
-        // if(cnt==0){
-        //     return true;
-        // }
-        // return false;
     }
 };
