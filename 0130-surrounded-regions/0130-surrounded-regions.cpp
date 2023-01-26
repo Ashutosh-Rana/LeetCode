aae10e90fb99d62412b3237bc12e16d4ab9638c9
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-01-26 15:36:29 +0530

    Time: 11 ms (92.04%), Space: 11.5 MB (17.94%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -20,22 +20,22 @@ public:
             if(!vis[0][i] && board[0][i]=='O'){
                 dfs(0,i,board,vis);
             }
-        }
-        for(int i=0;i<n;i++){
+            
             if(!vis[m-1][i] && board[m-1][i]=='O'){
                 dfs(m-1,i,board,vis);
             }
         }
+       
         for(int i=0;i<m;i++){
             if(!vis[i][0] && board[i][0]=='O'){
                 dfs(i,0,board,vis);
             }
-        }
-        for(int i=0;i<m;i++){
+            
             if(!vis[i][n-1] && board[i][n-1]=='O'){
                 dfs(i,n-1,board,vis);
             }
         }
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(!vis[i][j] && board[i][j]=='O'){
