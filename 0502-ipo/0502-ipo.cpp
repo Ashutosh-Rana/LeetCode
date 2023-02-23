0f39ebfd9b7faaa4e7954ca7da02c69de2d2590f
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-02-24 01:41:56 +0530

    Time: 279 ms (34.25%), Space: 82.1 MB (53.68%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n=profits.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minCapital;
+        for(int i=0;i<n;i++){
+            minCapital.push({capital[i],profits[i]});
+        }
+        priority_queue<int> maxProfit;
+        for(int i=0;i<k;i++){
+            while(!minCapital.empty() && minCapital.top().first<=w){
+                maxProfit.push(minCapital.top().second);
+                minCapital.pop();
+            }
+            if(maxProfit.empty()){
+                break;
+            }
+            w+=maxProfit.top();
+            maxProfit.pop();
+        }
+        return w;
+    }
+};
