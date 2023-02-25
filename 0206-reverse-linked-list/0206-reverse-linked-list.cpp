853ac931c95258776bdc1a2c6fa30b857f4b2f0e
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-02-25 17:09:20 +0530

    Time: 7 ms (68.91%), Space: 8.6 MB (11.33%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -9,18 +9,31 @@
  * };
  */
 class Solution {
+    private:
+    ListNode* Reverse(ListNode *head){
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode* newHead=Reverse(head->next);
+        ListNode* headNext=head->next;
+        headNext->next=head;
+        head->next=NULL;
+        return newHead;
+    }
 public:
     ListNode* reverseList(ListNode* head) {
         // if(head==NULL){
         //     return head;
         // }
-        ListNode *prev=NULL,*cur=head,*nextNode;
-        while(cur!=NULL){
-            nextNode=cur->next;
-            cur->next=prev;
-            prev=cur;
-            cur=nextNode;
-        }
-        return prev ;
+        // ListNode *prev=NULL,*cur=head,*nextNode;
+        // while(cur!=NULL){
+        //     nextNode=cur->next;
+        //     cur->next=prev;
+        //     prev=cur;
+        //     cur=nextNode;
+        // }
+        return Reverse(head);
+        //return prev ;
+        //return prev;
     }
 };
