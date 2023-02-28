de898756b3e090d8468d31e9cb4c912da7614512
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-03-01 02:19:47 +0530

    Time: 44 ms (64.80%), Space: 17.9 MB (51.33%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -23,40 +23,22 @@ class Solution {
 public:
     void reorderList(ListNode* head) {
         ListNode* temp=head;
-        // ListNode* res=new ListNode(0);
-        // ListNode* start=res;
-        
         ListNode *slow=head,*fast=head,*res=head;
-        // res->next=head;
         while(fast->next && fast->next->next){
             slow=slow->next;
             fast=fast->next->next;
         }
         ListNode* revList;
-        // cout<<slow->val;
         revList=Reverse(slow->next);
         slow->next=NULL;
-        // cout<<slow->next->val<<endl;
-        // cout<<revList->next->val;
         while(revList ){
-            // cout<<temp->val<<" "<<revList->val<<endl;
-            cout<<res->val<<" "<<revList->val<<endl;
+            // cout<<res->val<<" "<<revList->val<<endl;
             ListNode* temp2=revList;
             revList=revList->next;
-            // cout<<res->next->val<<endl;
             ListNode *nextNode=res->next;
             res->next=temp2;
             temp2->next=nextNode;
             res=res->next->next;
-            // cout<<res->val<<endl;
-            // res->next=revList;
-            // res=res->next;
-            // temp=temp->next;
-            // revList=revList->next;
-            // cout<<temp->val<<endl;
         }
-        // if(fast){
-        //     res->next=;
-        // }
     }
 };
