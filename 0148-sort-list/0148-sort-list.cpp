69f662345eae8d7758561414707c2bba84c4c268
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-03-11 03:17:12 +0530

    Time: 252 ms (24.97%), Space: 74 MB (28.04%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -9,25 +9,59 @@
  * };
  */
 class Solution {
-public:
-    ListNode* sortList(ListNode* head) {
-        if(!head){
-            return NULL;
-        }
-        vector<int> v;
-        ListNode* cur=head;
-        while(cur){
-            v.push_back(cur->val);
+private:
+    ListNode* mergeList(ListNode* l1,ListNode* l2){
+        ListNode* cur=new ListNode(0);
+        ListNode* res=cur;
+        while(l1 && l2){
+            if(l1->val<=l2->val){
+                cur->next=l1;
+                l1=l1->next;
+            }
+            else{
+                cur->next=l2;
+                l2=l2->next;
+            }
             cur=cur->next;
         }
-        sort(v.begin(),v.end());
-        ListNode* newHead = new ListNode(v[0]);
-        ListNode* res=newHead;
-        for(int i=1;i<v.size();i++){
-            ListNode* temp=new ListNode(v[i]);
-            newHead->next=temp;
-            newHead=newHead->next;
+        if(l1){
+            cur->next=l1;
         }
-        return res;
+        if(l2){
+            cur->next=l2;
+        }
+        return res->next;
+    }
+public:
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode *slow=head,*fast=head,*temp;
+        while(fast && fast->next){
+            temp=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        temp->next=NULL;
+        ListNode* left=sortList(head);
+        ListNode* right=sortList(slow);
+        return mergeList(left,right);
+        
+        // vector<int> v;
+        // ListNode* cur=head;
+        // while(cur){
+        //     v.push_back(cur->val);
+        //     cur=cur->next;
+        // }
+        // sort(v.begin(),v.end());
+        // ListNode* newHead = new ListNode(v[0]);
+        // ListNode* res=newHead;
+        // for(int i=1;i<v.size();i++){
+        //     ListNode* temp=new ListNode(v[i]);
+        //     newHead->next=temp;
+        //     newHead=newHead->next;
+        // }
+        // return res;
     }
 };
