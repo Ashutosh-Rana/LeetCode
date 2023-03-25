eefe93ee8c2b2bf340e04a6db773f48d9714f994
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-03-26 03:06:07 +0530

    Time: 554 ms (86.18%), Space: 179.4 MB (70.05%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,3 +1,4 @@
+#define ll long long
 class Solution {
 private:
     void dfs(int node,long long &cnt,vector<int>& vis,vector<int> adj[]){
@@ -16,11 +17,11 @@ private:
     }
 public:
     long long countPairs(int n, vector<vector<int>>& edges) {
-        if(edges.size()==0){
-            long long a=n;
-            long long res=((a*(a-1))/2);
-            return res;
-        }
+        // if(edges.size()==0){
+        //     long long a=n;
+        //     long long res=((a*(a-1))/2);
+        //     return res;
+        // }
         long long cnt;
         vector<int> vis(n,0);
         vector<long long> v;
@@ -29,20 +30,22 @@ public:
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
+        long long res=((ll)n*(n-1))/2;
         for(int i=0;i<n;i++){
             cnt=0;
             if(!vis[i]){
                 dfs(i,cnt,vis,adj);
-                v.push_back(cnt);
+                res-=(cnt*(cnt-1))/2;
+                // v.push_back(cnt);
                 // cout<<cnt<<endl;
             }
         }
-        long long res=0;
-        for(int i=0;i<v.size();i++){
-            for(int j=i+1;j<v.size();j++){
-                res+=v[i]*v[j];
-            }
-        }
+        
+        // for(int i=0;i<v.size();i++){
+        //     for(int j=i+1;j<v.size();j++){
+        //         res+=v[i]*v[j];
+        //     }
+        // }
         return res;
     }
 };
