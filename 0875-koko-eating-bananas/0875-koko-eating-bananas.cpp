8576e379c79d9e113a765616998630b2d52eb952
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-04-02 00:58:20 +0530

    Time: 60 ms (28.67%), Space: 19 MB (52.61%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(),piles.end());
+        int l=1,n=piles.size(),r=piles[n-1];
+        if(n==1){
+            return ceil(piles[0]/double(h));
+        }
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            // cout<<l<<" "<<mid<<" "<<r<<endl;
+            long sum=0;
+            for(int i=0;i<n;i++){
+                sum+=ceil(piles[i]/(double)mid);
+                // cout<<floor(piles[i]/(float)mid)<<" ";
+            }
+            // cout<<endl;
+            if(sum<=h){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return r+1;
+    }
+};
