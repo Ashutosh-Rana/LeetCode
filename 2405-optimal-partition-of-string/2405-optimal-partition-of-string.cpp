205a233fbd3a48ad103a1dc79bc28771141976ef
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-04-04 20:06:54 +0530

    Time: 326 ms (5.04%), Space: 88.1 MB (8.67%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -4,21 +4,32 @@ public:
         int n=s.size(),l=0,r=0;
         int cnt=1;
         unordered_map<char,int> mp;
-        while(r<n){
+//         while(r<n){
             
-            if(mp[s[r]]==0){
-                mp[s[r]]=1;
+//             if(mp[s[r]]==0){
+//                 mp[s[r]]=1;
                 
+//             }
+//             else{
+//                 l=r;
+//                 cnt++;
+//                 unordered_map<char,int> temp;
+//                 mp=temp;
+//                 mp[s[r]]=1;
+//             }
+//             // cout<<r<<" "<<mp[r]<<endl;
+//             r++;
+//         }
+        for(int i=0;i<n;i++){
+            if(mp[s[i]]==0){
+                mp[s[i]]=1;
             }
             else{
-                l=r;
                 cnt++;
                 unordered_map<char,int> temp;
                 mp=temp;
-                mp[s[r]]=1;
+                mp[s[i]]=1;
             }
-            // cout<<r<<" "<<mp[r]<<endl;
-            r++;
         }
         return cnt;
     }
