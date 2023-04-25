0d047aabe4003839bddefa99f0d1dc9ef66eb77e
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-04-25 11:32:20 +0530

    Time: 27 ms (35.00%), Space: 16.8 MB (87.83%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res=0;
+        for(int a:nums){
+            res=res^a;
+        }
+        return res;
+    }
+};
