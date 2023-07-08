bcf4a53e737715a202105b0665cd1bfd598d6ffa
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-07-08 15:41:29 +0530

    Time: 889 ms (88.58%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -4,5 +4,9 @@ from (
     select o.customer_number,(count(o.order_number)) as "cnt"
     from Orders o
     group by o.customer_number
-    order by cnt desc limit 1
+    having cnt=(
+        select max(c)
+        from (select count(*) as "c" from Orders group by customer_number) t
+        )
+    # order by cnt desc limit 1
     ) T;
