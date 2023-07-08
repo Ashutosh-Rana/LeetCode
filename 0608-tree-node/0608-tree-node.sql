7ac96c0999a16aa5747cebcc39fee00a95d0ab14
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-07-08 17:36:42 +0530

    Time: 811 ms (95.46%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -11,8 +11,8 @@
 select distinct t.id as id,
 case
     when isnull(t.p_id) then "Root"
-    when t.p_id is not null and t2.p_id is null then "Leaf"
-    when t.p_id is not null and t2.p_id is not null then "Inner"
+    when t.p_id is not null and t2.id is null then "Leaf"
+    when t.p_id is not null and t2.id is not null then "Inner"
     end as "type"
 from Tree t
 left join Tree t2 on t.id=t2.p_id;
