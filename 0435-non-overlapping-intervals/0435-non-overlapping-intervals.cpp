194d292e5f0254e08ddf0919efb6de2f594ff4b2
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-07-28 02:00:46 +0530

    Time: 336 ms (99.65%), Space: 89.7 MB (89.97%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         sort(intervals.begin(),intervals.end());
-        // cout<<intervals[1][0]<<intervals[1][1];
-        int start=intervals[0][0],end=intervals[0][1],cnt=0,n=intervals.size();
+        int n=intervals.size();
+        // for(int i=0;i<n;i++){
+        //     pq.push({intervals[i][0],intervals[i][1]});
+        // }
+        int start=intervals[0][0],end=intervals[0][1],res=0;
         for(int i=1;i<n;i++){
-            if(intervals[i][0]<end){
-                cnt++;
+            if(intervals[i][0]>=end){
+                start=intervals[i][0],end=intervals[i][1];
+            }
+            else{
+                res++;
                 if(end>intervals[i][1]){
                     start=intervals[i][0],end=intervals[i][1];
                 }
             }
-            else{
-                start=intervals[i][0],end=intervals[i][1];
-            }
+            // pq.pop();
         }
-        return cnt;
+        return res;
     }
 };
