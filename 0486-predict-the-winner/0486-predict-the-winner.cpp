98593ed0b0591893e9a4d9eb1dfecbcec8523f51
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-07-31 17:02:19 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (93.41%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,25 @@
 class Solution {
+    long long dp[20][20][2];
     long long helper(vector<int>& nums,int i,int j,int chance){
         if(i>j){
             return 0;
         }
+        if(dp[i][j][chance]!=-1){
+            return dp[i][j][chance];
+        }
         if(chance==0){
-            return max(nums[i]+helper(nums,i+1,j,1),nums[j]+helper(nums,i,j-1,1));
+            return dp[i][j][chance]=
+                max(nums[i]+helper(nums,i+1,j,1),nums[j]+helper(nums,i,j-1,1));
         }
         else{
-            return min(helper(nums,i+1,j,0),helper(nums,i,j-1,0));
+            return dp[i][j][chance]=
+                min(helper(nums,i+1,j,0),helper(nums,i,j-1,0));
         }
     }
 public:
     bool PredictTheWinner(vector<int>& nums) {
         long long total=0,p_two,n=nums.size();
+        memset(dp,-1,sizeof(dp));
         for(int i=0;i<nums.size();i++){
             total+=nums[i];
         }
