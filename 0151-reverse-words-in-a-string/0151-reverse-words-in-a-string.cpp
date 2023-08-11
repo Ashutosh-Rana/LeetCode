8827a71ff2f3f34ed947a339b17a99d7434c7e89
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-08-11 20:47:19 +0530

    Time: 3 ms (87.76%), Space: 20.7 MB (26.61%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> s2;
-        string tmp;
+        
+        string tmp,res;
         for(char c:s){
             if(c==' '){
                 if(tmp!=""){
-                    s2.push_back(tmp);
+                    tmp+=" ";
+                    res=tmp+res;
                 }
                 tmp="";
             }
@@ -15,13 +16,8 @@ public:
             }
         }
         if(tmp!=""){
-                    s2.push_back(tmp);
-                }
-        reverse(s2.begin(),s2.end());
-        string res;
-        for(string str:s2){
-            res+=str;
-            res+=' ';
+            tmp+=" ";
+            res=tmp+res;
         }
         res.erase(res.begin()+res.size()-1);
         return res;
