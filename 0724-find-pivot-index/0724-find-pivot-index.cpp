186975176b8bff0f00067aae429ef14a87ad0f14
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-08-16 02:27:33 +0530

    Time: 12 ms (96.46%), Space: 31.2 MB (41.61%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
-        vector<int> prefix(n),postfix(n);
-        prefix[0]=nums[0],postfix[n-1]=nums[n-1];
+        int pre=0,post=0;
+        for(int a:nums) post+=a;
+        post-=nums[0];
+        if(pre==post) return 0;
         for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-            postfix[n-i-1]=postfix[n-i]+nums[n-i-1];
+            pre+=nums[i-1];
+            post-=nums[i];
+            if(pre==post) return i;
         }
-        for(int i=0;i<n;i++){
-            if(prefix[i]==postfix[i]){
-                return i;
-            }
-        }
+        
         return -1;
     }
 };
