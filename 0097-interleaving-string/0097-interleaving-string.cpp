6ddd947b83f652882216e5c34b52cf3d5d6c451c
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-08-26 03:22:57 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (97.01%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int n1=s1.length(),n2=s2.length(),n3=s3.length();
-        if(n1+n2!=n3){
-            return false;
-        }
+        int n1=s1.size(),n2=s2.size(),n3=s3.size();
+        if(n1+n2!=n3) return false;
         bool dp[n1+1][n2+1];
-        for(int i=0;i<n1+1;i++){
-            for(int j=0;j<n2+1;j++){
+        for(int i=0;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
                 if(!i && !j){
                     dp[i][j]=true;
                 }
@@ -18,8 +16,8 @@ public:
                     dp[i][j]=dp[i-1][j] && s1[i-1]==s3[i+j-1];
                 }
                 else{
-                    dp[i][j]=(dp[i][j-1] && s2[j-1]==s3[i+j-1]) or 
-                        (dp[i-1][j] && s1[i-1]==s3[i+j-1]);
+                    dp[i][j]=(dp[i-1][j] && s1[i-1]==s3[i+j-1]) or
+                             ( dp[i][j-1] && s2[j-1]==s3[i+j-1] );
                 }
             }
         }
