9555368886dbc8949396f036548d0c6216d2cb0c
ASHUTOSH RANA <ashutoshrana978@gmail.com>
2023-09-05 12:04:06 +0530

    Time: 12 ms (18.70%), Space: 11.5 MB (15.63%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,18 +17,24 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* cur=head;
         unordered_map<Node*,Node*> mp;
+        if(!head) return NULL;
+        Node* cur=head;
         while(cur){
             mp[cur]=new Node(cur->val);
             cur=cur->next;
         }
-        cur=head;
-        while(cur){
-            mp[cur]->next=mp[cur->next];
-            mp[cur]->random=mp[cur->random];
-            cur=cur->next;
+        int cur_val=head->val;
+        Node *cur2=head;
+        mp[cur2]=new Node(cur_val);
+        Node *res=mp[cur2];
+        while(cur2){
+            // cout<<res->val<<endl;
+            mp[cur2]->next=mp[cur2->next];
+            mp[cur2]->random=mp[cur2->random];
+            cur2=cur2->next;
+            // res=res->next;
         }
-        return mp[head];
+        return res;
     }
 };
